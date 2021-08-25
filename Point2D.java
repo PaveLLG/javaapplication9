82a06c0c1a6a7c3f5a67895bb49310f6d263cd9e
PaveL <BPaveL07@yandex.ru>
2021-08-25 12:05:08 +0300

    HI

--- Point2D.java
+++ Point2D.java
@@ -7,15 +7,15 @@ public class Point2D {
     }
 
     public Point2D(int x, int y) {
-        this.x = x;
-        this.y = y;
+        setX(x);
+        setY(y);
     }
 
     public int getX() {
         return x;
     }
 
-    public void setX(int x) {
+    public final void setX(int x) {
         this.x = x;
     }
 
@@ -23,7 +23,7 @@ public class Point2D {
         return y;
     }
 
-    public void setY(int y) {
+    public final void setY(int y) {
         this.y = y;
     }
 
