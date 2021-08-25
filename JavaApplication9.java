d0cdfbdf82bc557218fed245a69139b31f1556b8
PaveL <BPaveL07@yandex.ru>
2021-08-25 11:54:54 +0300

    HI

--- JavaApplication9.java
+++ JavaApplication9.java
@@ -2,12 +2,13 @@ public class JavaApplication9 {
 
     public static void main(String[] args){
 
-        Point2D point2D = new Point2D(0,0);
+        Point2D point2D = new Point2D();
         point2D.setX(100);
         point2D.setY(2200);
 //Привет
         System.out.println(point2D);
 
         Point2D p2 = new Point2D(2, 4);
+        System.out.println(p2);
     }
 }
--- Point2D.java
+++ Point2D.java
@@ -2,6 +2,10 @@ public class Point2D {
 
     private int x,y;
 
+    public Point2D(){
+        this(0,0);
+    }
+
     public Point2D(int x, int y) {
         this.x = x;
         this.y = y;
