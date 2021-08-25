f082a1c4b8a91c9af95fbbc4acd3dab7665949d4
PaveL <BPaveL07@yandex.ru>
2021-08-25 11:44:33 +0300

    HI

--- JavaApplication9.java
+++ JavaApplication9.java
@@ -2,10 +2,12 @@ public class JavaApplication9 {
 
     public static void main(String[] args){
 
-        Point2D point2D = new Point2D();
+        Point2D point2D = new Point2D(0,0);
         point2D.setX(100);
         point2D.setY(2200);
 //Привет
         System.out.println(point2D);
+
+        Point2D p2 = new Point2D(2, 4);
     }
 }
